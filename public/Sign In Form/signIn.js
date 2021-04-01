32cae355126020ba42304a5b4cdd96b2cf8b69da
Paul Jreij <pjreij98@gmail.com>
2021-03-31 20:49:53 -0500

    sign in and registration

--- public/Sign In Form/signIn.js
+++ public/Sign In Form/signIn.js
@@ -1,16 +1,21 @@
-function validate()
+function register()
 {
     var email = document.getElementById("email").value;
     var password = document.getElementById("password").value;
 
-    if(email == "admin@gmail.com" && password == "pass")
-    {
-        alert("login successful");
-        true;
-    }
-    else
-    {
-        alert("login failed");
-        return false;
-    }
+    const promise = auth.createUserWithEmailAndPassword(email.value, password.value);
+    promise.catch(e => alert(e.message));
+    console.log("Hello");
+    alert("Registered");
+}
+
+function signIn()
+{
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+
+    const promise = auth.signInWithEmailAndPassword(email.value, password.value);
+    promise.catch(e => alert(e.message));
+
+    alert("Signed In " + email);
 }
