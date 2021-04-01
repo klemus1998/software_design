8386395075fcb8093494755ddbcd5c49cd54273e
Christopher Yee <chris.yee43@gmail.com>
2021-03-31 23:06:13 -0500

    fuel quote database stuff

--- public/Fuel_Quote_Form/fuel_quote.js
+++ public/Fuel_Quote_Form/fuel_quote.js
@@ -1,23 +1,45 @@
-function handleSubmitHistory() {
-    var gallons = document.getElementById("gallons_id").value;
-    var deliverydate = document.getElementById('test').value;
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyALuDn_Og_wckAe6JclRKlDzzk2Rim-4Pw",
+    authDomain: "software-design-8ea1b.firebaseapp.com",
+    databaseURL: "https://software-design-8ea1b-default-rtdb.firebaseio.com",
+    projectId: "software-design-8ea1b",
+    storageBucket: "software-design-8ea1b.appspot.com",
+    messagingSenderId: "357419323251",
+    appId: "1:357419323251:web:707639ce6ca1dc738aff06",
+    measurementId: "G-MWFFLG1JZE"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
 
-    if (typeof Number(gallons) == 'number') {
-        localStorage.setItem('gallons_id', gallons);
-    }
-    
-    localStorage.setItem("test", deliverydate);
-    localStorage.setItem("PRICE/GALLON", pricing_module());
-    localStorage.setItem("total", calc_result());
+var userRef = firebase.database().ref("users/fuelquote");
 
-    return;
+document.getElementById('fuelquote').addEventListener('submit', submitForm);
+
+function submitForm(e) {
+    e.preventDefault();
+
+    var gallons = document.getElementById('gallons_id').value;
+    var date = document.getElementById('date').value;
+
+    saveData(gallons, date);
+
+    window.location.href = "../Quote History/fuel_history.html";
 }
+
+function saveData(gallons, date) {
+    userRef.push().update({
+        gallons_requested: gallons,
+        delivery_date: date
+    });
+}
+
 
 window.addEventListener('load', () =>
 {
-    d_address.value = localStorage.getItem('ADDRESS');
-
-    document.getElementById("d_address").innerHTML = d_address.value;
+    
 })
 
 function pricing_module() {
